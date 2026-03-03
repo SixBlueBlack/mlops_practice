1d96c56af3a8bfa5af13e166039e4a4f969e1325
mifix013 <mifix013@gmail.com>
2026-03-03 11:58:39 +0500

    Update test_predict.py

--- lab3/test_predict.py
+++ lab3/test_predict.py
@@ -1,1 +1,41 @@
+import os
+import unittest
+from app import predict_emotion, predict_emotions
+from threading import Thread
+from time import sleep
+from requests.api import get
 
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.predict_data = {
+            'Привет!': 'happiness',
+            'Ненавижу выходить на улицу в дождь!!!': 'anger',
+            'Мне очень грустно, когда много домашнего задания': 'sadness',
+            'Может сходим погулять?': 'enthusiasm',
+            'Я боюсь темноты': 'fear',
+            'Сегодня обычный день': 'neutral',
+            'disgust': 'disgust',
+        }
+
+    def test_predict(self):
+        for k, v in filter((lambda x: x[1] != 'disgust'), self.predict_data.items()):
+            self.assertEqual(predict_emotion(k), v)
+
+    def test_sum_predict(self):
+        for k in self.predict_data.keys():
+            result = sum(predict_emotions(k).values())
+            self.assertAlmostEqual(round(result, 1), 1.0, places=1)
+
+    def test_keys_predict(self):
+        self.assertEqual(sorted(predict_emotions('Тест').keys()), sorted(self.predict_data.values()))
+
+    def test_api(self):
+        t = Thread(target=os.system, args=('streamlit run app.py --server.port 1460',))
+        t.daemon = True
+        t.start()
+        sleep(5)
+        response = get('http://127.0.0.1:1460')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()

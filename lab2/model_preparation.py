161797e801ec0d0113ee2788b8f08107bb311efc
mifix013 <mifix013@gmail.com>
2026-03-03 11:26:11 +0500

    Update model_preparation.py

--- lab2/model_preparation.py
+++ lab2/model_preparation.py
@@ -1,1 +1,31 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import os
+import joblib
 
+os.makedirs('train/preprocessed', exist_ok=True)
+os.makedirs('test/preprocessed', exist_ok=True)
+
+train_data = pd.read_csv("train/digits_train.csv")
+test_data = pd.read_csv("test/digits_test.csv")
+
+X_train = train_data.drop(columns=['label'])
+y_train = train_data['label']
+
+X_test = test_data.drop(columns=['label'])
+y_test = test_data['label']
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+train_preprocessed = pd.DataFrame(X_train_scaled, columns=X_train.columns)
+train_preprocessed.insert(0, 'label', y_train.values)
+train_preprocessed.to_csv('train/preprocessed/digits_train_preprocessed.csv', index=False)
+
+test_preprocessed = pd.DataFrame(X_test_scaled, columns=X_test.columns)
+test_preprocessed.insert(0, 'label', y_test.values)
+test_preprocessed.to_csv('test/preprocessed/digits_test_preprocessed.csv', index=False)
+
+joblib.dump(scaler, 'scaler.pkl')
+print("Предобработка данных завершена")
